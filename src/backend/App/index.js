0bcdc4e9a86e4b7218a1bfda5e22549535abc8b9
sprieto <santi.prieto1212@gmail.com>
2021-04-11 14:46:57 -0500

    adding express

--- src/backend/App/index.js
+++ src/backend/App/index.js
@@ -1,4 +1,9 @@
-const serverless= requ
-exports.handler=async (event,context)=>{
-    console.log("Hola", event)
-}
+const serverless = require("serverless-http");
+const server = require("./app");
+
+const { PORT } = process.env;
+const port = parseInt(PORT, 10) || 4008;
+
+server.listen(port, () => `App started listening at ${port}`);
+
+exports.handler = serverless(server);
