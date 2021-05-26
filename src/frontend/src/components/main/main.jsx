54106cac3769ccec9a83e326cf383f40fc903a8f
unknown <santi.prieto1212@gmail.com>
2021-05-25 19:06:11 -0500

    document things

--- src/frontend/src/components/main/main.jsx
+++ src/frontend/src/components/main/main.jsx
@@ -12,6 +12,9 @@ import {
   SearchBarForm,
   SearchBarInput,
   SearchBarContainer,
+  Details,
+  Summary,
+  HR,
 } from "./styled";
 
 const Main = () => {
@@ -19,20 +22,26 @@ const Main = () => {
     <ProyectContainer id="services">
       <ProyectsH1>PROYECTOS</ProyectsH1>
       <SearchBar />
-      <ProyectWrapper>
-        <ProyectsCard>
-          <ProyectsIcon src={"./img/6nuevoproyecto_192x192.png"} />
-          <ProyectsH2>Nuevo Proyecto</ProyectsH2>
-        </ProyectsCard>
-        <ProyectsCard>
-          <ProyectsIcon src={"./img/7nuevoproyecto_192x192.png"} />
-          <ProyectsH2>Proyectos2</ProyectsH2>
-        </ProyectsCard>
-        <ProyectsCard>
-          <ProyectsIcon src={"./img/nuevoproyecto_192x192.png"} />
-          <ProyectsH2>Proyectos6</ProyectsH2>
-        </ProyectsCard>
-      </ProyectWrapper>
+      <Collapse Title="Mis proyectos">
+        <ProyectWrapper>
+          <ProyectsCard>
+            <ProyectsIcon src={"./img/6nuevoproyecto_192x192.png"} />
+            <ProyectsH2>Nuevo Proyecto</ProyectsH2>
+          </ProyectsCard>
+          <ProyectsCard></ProyectsCard>
+          <ProyectsCard></ProyectsCard>
+        </ProyectWrapper>
+      </Collapse>
+      <Collapse Title="Todos Los Proyectos">
+        <ProyectWrapper>
+          <ProyectsCard>
+            <ProyectsIcon src={"./img/6nuevoproyecto_192x192.png"} />
+            <ProyectsH2>Nuevo Proyecto</ProyectsH2>
+          </ProyectsCard>
+          <ProyectsCard></ProyectsCard>
+          <ProyectsCard></ProyectsCard>
+        </ProyectWrapper>
+      </Collapse>
     </ProyectContainer>
   );
 };
@@ -54,7 +63,10 @@ function SearchBar() {
   return (
     <SearchBarContainer>
       <SearchBarForm>
-        <SearchBarButton type="submit"> <FaSearch/></SearchBarButton>
+        <SearchBarButton type="submit">
+          {" "}
+          <FaSearch />
+        </SearchBarButton>
         <SearchBarInput
           onChange={(e) => setInput(e.target.value)}
           ref={inputFocus}
@@ -65,3 +77,16 @@ function SearchBar() {
     </SearchBarContainer>
   );
 }
+
+const Collapse = (props) => {
+  const { Title, children } = props;
+  return (
+    <Details open={true}>
+      <Summary>
+        {Title}
+        <HR />
+      </Summary>
+      {children}
+    </Details>
+  );
+};
